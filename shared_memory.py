301f7e7a5cff3295046c232abb61935a3b57def6
jacopo binosi <jacopo.binosi@musixmatch.com>
2022-09-01 16:24:44 +0000

    wip

--- shared_memory.py
+++ shared_memory.py
@@ -1,5 +1,6 @@
 from multiprocessing import shared_memory
-
+from jobber import Jobber
+import time
 
 class JobberSHM:
     def __init__(self, size: int):
@@ -24,3 +25,59 @@ class JobberSHM:
         self._shm.close()
         self._shm.unlink()
 
+
+class JSubscriber:
+    def __init__(self, jshm: JobberSHM):
+        self._shm = jshm
+        self._word: str = ''
+
+    def listen(self):
+        word = ''
+        while word != 'SHUTDOWN':
+            word = jshm.get()
+            if word != self._word:
+                print(f'{word} != {self._word}')
+                self._word = word
+        jshm.close()
+
+class JPublisher:
+    def __init__(self, jshm: JobberSHM):
+        self._shm = jshm
+
+    def publish(self, word: str):
+        self._shm.put(word)
+
+    def shutdown(self):
+        self._shm.put('SHUTDOWN')
+
+# set concunrrency (best is number of cores)
+# and initialize Jobber
+concurrency = 2
+jobber = Jobber(concurrency)
+
+# get the decorator
+jobberd = jobber.decorator
+
+jshm = JobberSHM(32)
+pub = JPublisher(jshm)
+sub1 = JSubscriber(jshm)
+sub2 = JSubscriber(jshm)
+
+@jobberd
+def listener1():
+    sub1.listen()
+
+@jobberd
+def listener2():
+    sub2.listen()
+
+
+if __name__ == '__main__':
+    jobber.work()
+
+    time.sleep(3)
+    for i in range(3):
+        pub.publish(f'Hello {i}')
+        time.sleep(3)
+
+    pub.shutdown()
